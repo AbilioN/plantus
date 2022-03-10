5c8d9ad72e18b65928f143168494ee1bd126e7c3
root <root@DESKTOP-OSR3RH1.localdomain>
2022-03-10 06:51:03 -0300

    fix: refactored model

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -3,33 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
 
 class UserAddress extends Model
 {
-     // private User $user;
-
-     protected $table = 'user_addresses';
-     protected $fillable  = [ 'user_id' , 'street' , 'number' , 'cep' , 'neighborhood' , 'state' , 'city' ,'adjunct'];
-
-     public function __construct()
-     {
-          // $this->user = Auth::user();
-          // $this->user = User::find(1);
-     }
-
-     // public function createUserAddress(array $data) {
-
-
-     //      $this->create([
-     //           'user_id' => 1,
-     //           'street' => $data['street'],
-     //           'number' => $data['number'],
-     //           'cep' => $data['cep'],
-     //           'neighborhood' => $data['neighborhood'],
-     //           'state' => $data['state'],
-     //           'city' => $data['city'],
-     //           'adjunct' => $data['adjunct'],
-     //   ]);
-     // }
+    protected $fillable = ['user_id' , 'street' , 'number' , 'cep' , 'neighborhood' , 'state' , 'city' , 'adjunct'];
 }
