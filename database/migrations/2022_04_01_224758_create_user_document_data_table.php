c053af9601bda722bf02da9ab7d52018686afc55
Abílio Neto <netobalby@gmail.com>
2022-04-10 10:17:39 -0300

    add user document data

--- database/migrations/2022_04_01_224758_create_user_document_data_table.php
+++ database/migrations/2022_04_01_224758_create_user_document_data_table.php
@@ -21,8 +21,7 @@ class CreateUserDocumentDataTable extends Migration
             $table->string('father')->nullable();
             $table->string('bank_account');
 
-            $table->unsignedBigInteger('user_id');
-
+            $table->unsignedBigInteger('user_id');                            
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
