736e64a2da4f30e69567f425bfe91c085ee55060
AbilioN <netobalby@gmail.com>
2022-04-26 09:12:45 -0300

    added team show routing

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -3,14 +3,39 @@
 namespace App\Http\Controllers;
 
 use App\Models\Team;
+use App\Models\UsersTeam;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TeamController extends Controller
 {
     
     public function show(Request $request)
     {
+
+        $outputArray = [];
         $teams = Team::all();
-        dd($teams);
+        foreach($teams as $team)
+        {
+            $usersTeam = DB::table('users_teams')
+                            ->select(
+                                'users.name',
+                                'users.email',
+                                'users.phone',
+                                'users.whatsapp',
+                                'roles.role'
+
+                            )
+                            ->join('users', 'users.id', '=' , 'users_teams.user_id')
+                            ->join('user_roles', 'user_roles.user_id', '=' , 'users.id')
+                            ->join('roles', 'user_roles.role_id', '=' ,'roles.id')
+                            ->get();
+            
+            $outputArray[$team->team] = collect($usersTeam)->toArray();
+        }
+
+        
+        return response()->json($outputArray);
+
     }
 }
